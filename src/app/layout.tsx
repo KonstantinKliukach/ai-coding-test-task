94e1fea48cb2783af2fc65df47f90b8d158020e2
Konstantin Kliukach <konstantin.kliukach@telekom.com>
2023-11-24 15:06:03 +0200

    MUI base set-up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+import AppTopBar from '@/components/AppTopBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeRegistry>
+          <AppTopBar />
+          {children}
+        </ThemeRegistry>
+      </body>
     </html>
   );
 }
